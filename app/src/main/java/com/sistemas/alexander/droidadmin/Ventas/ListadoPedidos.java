7e84f6e0521953aa204bdd346d90647ed3e3915e
AlexanderSS <alex_990_1@hotmail.com>
2015-06-29 23:39:08 -0500

    Commit 29/06/2015

---
+++ app/src/main/java/com/sistemas/alexander/droidadmin/Ventas/ListadoPedidos.java
@@ -0,0 +1,22 @@
+package com.sistemas.alexander.droidadmin.Ventas;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sistemas.alexander.droidadmin.R;
+
+/**
+ * Created by Alexander on 29/06/2015.
+ */
+public class ListadoPedidos extends Fragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.ventas_listpedido, container, false);
+        return rootView;
+    }
+}
