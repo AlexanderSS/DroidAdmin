ca95fb64b8c134b67160217c958fcb430174f83a
AlexanderSS <alex_990_1@hotmail.com>
2015-06-29 23:20:36 -0500

    Commit 29/06/2015

--- app/src/main/java/com/sistemas/alexander/droidadmin/VentasActivity.java
+++ app/src/main/java/com/sistemas/alexander/droidadmin/VentasActivity.java
@@ -1,10 +1,11 @@
 package com.sistemas.alexander.droidadmin;
 
 import android.app.Activity;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
@@ -17,6 +18,11 @@ import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
+import com.sistemas.alexander.droidadmin.Ventas.ListadoClientes;
+import com.sistemas.alexander.droidadmin.Ventas.ListadoPedidos;
+import com.sistemas.alexander.droidadmin.Ventas.RegistroClientes;
+import com.sistemas.alexander.droidadmin.Ventas.RegistroPedidos;
 
 
 public class VentasActivity extends ActionBarActivity
@@ -50,9 +56,24 @@ public class VentasActivity extends ActionBarActivity
     @Override
     public void onNavigationDrawerItemSelected(int position) {
         // update the main content by replacing fragments
-        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentManager fragmentManager = getFragmentManager();
+        Fragment fragment = null;
+        switch (position){
+            case 0:
+                fragment = new RegistroClientes();
+                break;
+            case 1:
+                fragment = new ListadoClientes();
+                break;
+            case 2:
+                fragment = new RegistroPedidos();
+                break;
+            case 3:
+                fragment = new ListadoPedidos();
+                break;
+        }
         fragmentManager.beginTransaction()
-                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
+                .replace(R.id.container, fragment)
                 .commit();
     }
 
@@ -66,6 +87,9 @@ public class VentasActivity extends ActionBarActivity
                 break;
             case 3:
                 mTitle = getString(R.string.title_section3);
+                break;
+            case 4:
+                mTitle = getString(R.string.title_section4);
                 break;
         }
     }
@@ -104,46 +128,6 @@ public class VentasActivity extends ActionBarActivity
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_ventas, container, false);
-            return rootView;
-        }
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((VentasActivity) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
-        }
     }
 
 }
